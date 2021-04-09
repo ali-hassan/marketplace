cf0b83b38c121c72b871864a253fd992b6ab64c1
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-04-09 10:25:51 -0400

    Revverted some files as they don't needed for production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Add folder with webpack generated assets to assets.paths
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts","webpack")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpack")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
