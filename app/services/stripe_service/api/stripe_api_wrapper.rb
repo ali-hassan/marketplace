27f2e7692b46ad0d97b79f8705cf8e95ea987291
Ali Hassan Mirza <ali.hassan.mirzaa+100@gmail.com>
2021-07-07 06:53:05 -0400

    Changge for rrecursive payment

--- app/services/stripe_service/api/stripe_api_wrapper.rb
+++ app/services/stripe_service/api/stripe_api_wrapper.rb
@@ -262,7 +262,7 @@ class StripeService::API::StripeApiWrapper
           currency: currency,
           confirmation_method: 'manual',
           setup_future_usage: 'off_session',
-          save_payment_method: true,
+          # save_payment_method: true,
           confirm: true,
           on_behalf_of: seller_account_id,
           transfer_data: {
